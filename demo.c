a102a0733571e0a07bb0686da01ccefc5b165340
abdullah-027 <abdullahkhan2020027@gmail.com>
2023-06-12 12:19:41 +0800

    demo updated

--- demo.c
+++ demo.c
@@ -1,1 +1,15 @@
 //Hi I hope all of you are doing well this is just a demo file
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+		char name[50];
+		name = "Abdullah Khan";
+		printf("My name is: %s",name);
+
+		return 0;
+}
